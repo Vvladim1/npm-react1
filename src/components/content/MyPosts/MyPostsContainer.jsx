8a845b57677afac88545662f855bed0e5f4e9939
vladimir <vladim.volovenko@gmail.com>
2020-02-16 23:30:09 +0200

    connect, mapStateToProps, mapDispatchTooProps Maked a copy of the state in ProfileReducer

--- src/components/content/MyPosts/MyPostsContainer.jsx
+++ src/components/content/MyPosts/MyPostsContainer.jsx
@@ -1,26 +1,50 @@
 import React from "react";
 import { addPostActionCreator, updateNewPostTextActionCreator } from "../../redux/profile-reducer";
 import MyPosts from "./MyPosts";
+import { connect } from "react-redux";
 
-const MyPostsContainer = props => {
-    let state = props.store.getState().profilePage;
+// const MyPostsContainer = props => {
+//     let state = props.store.getState().profilePage;
 
-  let addPost = () => {
-      props.store.dispatch(addPostActionCreator());
-  };
+//   let addPost = () => {
+//       props.store.dispatch(addPostActionCreator());
+//   };
 
-  let onPostChange = (text) => {
-    let action = updateNewPostTextActionCreator(text);
-    props.store.dispatch(action);
-  };
+//   let onPostChange = (text) => {
+//     let action = updateNewPostTextActionCreator(text);
+//     props.store.dispatch(action);
+//   };
 
-  return (
-      <MyPosts addPost={addPost}
-      onPostChange={onPostChange}
-      posts={state.posts}
-      newPostText={state.newPostText}
-       />
-  );
-};
+//   return (
+//       <MyPosts addPost={addPost}
+//       onPostChange={onPostChange}
+//       posts={state.posts}
+//       newPostText={state.newPostText}
+//        />
+//   );
+// };
+
+let mapStateToProps = (state) => {
+  return {
+    posts: state.profilePage.posts,
+    newPostText: state.profilePage.newPostText
+
+  }
+}
+let mapDispatchToProps = (dispatch) => {
+  return {
+    addPost: () => {
+      dispatch(addPostActionCreator());
+    },
+
+    onPostChange: (text) => {
+      let action = updateNewPostTextActionCreator(text);
+      dispatch(action);
+    }
+  }
+}
+
+
+const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
 
 export default MyPostsContainer;
--- src/components/redux/profile-reducer.js
+++ src/components/redux/profile-reducer.js
@@ -20,14 +20,18 @@ const profileReducer = (state = initialState, action) => {
         message: state.newPostText,
         likesCounte: 0
       };
+      let stateCopy = {...state};
+      stateCopy.posts = [...state.posts]
       if (newPost.message != "") {
-        state.posts.push(newPost);
-        state.newPostText = "";
+        stateCopy.posts.push(newPost);
+        stateCopy.newPostText = "";
       }
-      return state;
-    case UPDATE_NEW_POST_TEXT:
-      state.newPostText = action.newText;
-      return state;
+      return stateCopy;
+    case UPDATE_NEW_POST_TEXT:{
+      let stateCopy = {...state};
+      stateCopy.newPostText = action.newText;
+      return stateCopy;
+    }
     default:
       return state;
   }
