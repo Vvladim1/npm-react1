e26f7fe43dd345d206ba523501e46765c73ffdaf
vladimir <vladim.volovenko@gmail.com>
2020-02-25 00:05:21 +0200

    life cycle methods, componentDidMount, React, Redux

--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -5,9 +5,11 @@ import userPhoto from "../../assets/images/userImg.jpg";
 
 class Users extends React.Component {
 
-  constructor(props){
-    super(props);
+  // constructor(props){
+  //   super(props);
+  //   }
 
+    componentDidMount() {
       axios
         .get("https://social-network.samuraijs.com/api/1.0/users")
         .then(response => {
